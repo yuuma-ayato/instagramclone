7b6ee9de047042a1eba5a981d3af97c5d330b122
akihiro tokuhara <muscletown_mcm@yahoo.co.jp>
2020-06-13 01:09:09 +0900

    プロフィール編集機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,33 @@
 class UsersController < ApplicationController
 
-  def show
-    @user = User.find(params[:id])
-  end
-
   def new
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
-    if params[:back]
-      render :new
+    if @user.save
+      redirect_to user_path(@user.id)
     else
-      if @user.save
-        redirect_to user_path(@user.id)
-      else
-        render :new
-      end
+      render :new
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+      if @user.update(user_params)
+        redirect_to user_path, notice: "編集しました！"
+      else
+        render :show
+      end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
 
@@ -26,7 +35,7 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-      :password_confirmation,
-      :image, :image_cache)
+                                 :password_confirmation,
+                                 :image, :image_cache)
     end
   end
