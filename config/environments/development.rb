ddb8550f71ac7d1bbe8dcd113ade0dfae523a3c1
akihiro tokuhara <muscletown_mcm@yahoo.co.jp>
2020-06-15 11:09:06 +0900

    開発環境でletter_opener_webを使用するように設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,4 +32,8 @@ Rails.application.configure do
   config.assets.quiet = true
 
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
+  config.action_mailer.delivery_method = :letter_opener_web
 end
